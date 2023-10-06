5563172b72a0a102ae8df60edab9c4919dd5dda9
Puguh Rismadi Ismail <puguh.rismadi@gmail.com>
2023-10-06 22:26:38 +0700

    my first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API Guest Book</title>
+</head>
+<body>
+    <h2>Guest Book API</h2>
+</body>
+</html>
